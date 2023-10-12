df0bf950c5041d54215a359ace05c47abf40b382
kylvowray6 <kylvowray6@gmail.com>
2023-10-12 14:07:21 -0700

    k

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,7 +3,7 @@
 
 /**
  * add_dnodeint - Adds a new node at the beginning of a dlistint_t list
- * @head: Pointer to the head of the doubly linked list
+ * @head: Pointer to a pointer to the head of the doubly linked list
  * @n: Value to be stored in the new node
  * Return: The address of the new element, or NULL if it failed
  */
